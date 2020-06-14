fe19b62b438a5e453406c464a2d1bfd52017cbda
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-14 22:33:24 +0200

    login integration done

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -71,7 +71,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Adresse e-mail introuvable.');
+            throw new CustomUserMessageAuthenticationException('Email adress not found.');
         }
 
         return $user;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['MuseoModerno', 'sans-serif'],
+      'global': ['Raleway', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        fof: "#F0f"
+      }
+    },
   },
   variants: {},
   plugins: [],
