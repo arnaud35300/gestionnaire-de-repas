43f35f1fe6f5130e50a81f28e2b2ffb01a7e65e3
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-15 00:04:27 +0200

    update signup form and signup styles done

--- src/Form/UserAddType.php
+++ src/Form/UserAddType.php
@@ -5,17 +5,76 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserAddType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('password')
-        ;
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Joe'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a firstname'
+                    ]),
+                    new Length([
+                        'min' => 1,
+                        'minMessage' => 'Your firstname should be at least {{ limit }} characters',
+                        'max' => 40,
+                        'maxMessage' => 'Your firstname must be shorter than {{ limit }} characters'
+                    ]),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'you@example.com'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Your email should be at least {{ limit }} characters',
+                        'max' => 50,
+                        'maxMessage' => 'Your email must be shorter than {{ limit }} characters'
+                    ]),
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => [
+                    'placeholder' => 'must be at least 6 caracters'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password'
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'max' => 40,
+                        'maxMessage' => 'Your password must be shorter than {{ limit }} characters'
+                    ]),
+                ]
+            ])
+            ->add('agree_terms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
