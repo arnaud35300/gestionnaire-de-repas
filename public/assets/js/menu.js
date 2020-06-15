9650c9054486f98333bbcde4f77b126bec23aeef
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-15 04:29:09 +0200

    profile integration done

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,23 +1,27 @@
+const app = {
+    menu: document.querySelector("#menu"),
+    init: () => {
+        let button = document.querySelector('#button')
+        var x = true
 
-document.addEventListener('DOMContentLoaded', () => {
-    let button = document.querySelector('#button')
-    button.addEventListener('click', () => {
-        let menu = document.querySelector("#menu")
-        let display = menu.style.display === "none" ? menu.style.display = "block" : menu.style.display = "none"
-    })
-    var x = true
-    window.addEventListener('resize', () => {
-        let menu = document.querySelector("#menu")
-        
-        if (window.innerWidth < 1024 && x) {
+        if (window.innerWidth < 1024)
             menu.style.display = 'none'
-            x = false
-        }
-        if (window.innerWidth >= 1024) {
-            menu.style.display = 'flex'
-            x = true
-        }
-    });
-})
 
-console.log('edd')
+        button.addEventListener('click', () => {
+            let display = menu.style.display === "none" ? menu.style.display = "block" : menu.style.display = "none"
+        })
+
+        window.addEventListener('resize', () => {
+            if (window.innerWidth < 1024 && x) {
+                menu.style.display = 'none'
+                x = false
+            }
+            if (window.innerWidth >= 1024) {
+                menu.style.display = 'flex'
+                x = true
+            }
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
