a020381b13394e62c58e8ffae472dac631b80448
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-15 23:16:26 +0200

    create user password form

---
+++ src/Form/UserEditPasswordType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserEditPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('name')
+            ->add('updatedAt')
+            ->add('createdAt')
+            ->add('path')
+            ->add('role')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
