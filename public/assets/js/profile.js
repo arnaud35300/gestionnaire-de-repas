26be7418858f97b9c3f75af6436a1064593821ae
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-17 15:43:42 +0200

    display message error

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,13 +1,32 @@
 var profileApp = {
-    helpForm: document.querySelector('#message-form'),
-    contactForm: document.querySelector('#contact-form'),
     init: () => {
+        const forms = document.querySelectorAll('.ajax')
 
-        profileApp.helpForm.addEventListener('submit', profileApp.handleSubmitForm())
-        profileApp.contactForm.addEventListener('submit', profileApp.handleSubmitForm())
+        for (i = 0; i < forms.length; i++)
+            forms[i].addEventListener('submit', profileApp.handleSubmitForm)
     },
-    handleSubmitForm: () => function() {
+    handleSubmitForm: (e) => {
+        e.preventDefault()
 
+        const form = e.currentTarget
+        const input = form.querySelector('textarea')
+
+        if (input.value.trim() === '' || input.value.length > 200) {
+            profileApp.displayError('Your message must be between 5 and 200 characters', form)
+            return
+        }
+        profileApp.fetchFormData(input.value, form.id)
+    },
+    displayError: (message, form) => {
+
+        const divError = form.querySelector('#error')
+        divError.innerHTML = `
+        <div class="bg-red-700 text-white rounded-md font-bold p-3 mr-2">
+            ${message}
+        </div>`
+    },
+    fetchFormData: (data, path) => {
+        
     }
 }
 
