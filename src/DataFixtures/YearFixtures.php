a3c0341361211a6d50eddf5152828dc4f7f60d2a
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-06-19 23:02:28 +0200

    Year fixtures

---
+++ src/DataFixtures/YearFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Role;
+use App\Entity\Year;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class YearFixtures extends Fixture implements FixtureGroupInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $currentDate = new \DateTime();
+        $year = new Year();
+        $year
+            ->setName(
+                (int) (new \DateTime)->format('Y')
+            )
+            ->setCreatedAt(new \DateTime());
+
+        $manager->persist($year);
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['date-group'];
+    }
+}
